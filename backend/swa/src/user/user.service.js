cfb206f3d8837c8d9a77a3d8ab737b841e02d4a1
bmm333 <benmema3@gmail.com>
2025-05-19 22:23:21 +0200

    Redefining Code Structure making sure we follow good engineering principles

--- backend/swa/src/user/user.service.js
+++ backend/swa/src/user/user.service.js
@@ -1,6 +1,5 @@
 import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 
