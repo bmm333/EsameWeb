6fe32cbbbed9be2ce00e74d6ab444eb629f83cea
bmm333 <benmema3@gmail.com>
2025-05-23 23:53:14 +0200

    Readjusted auth: CODE WORKS IT DIDNT BREAK :D

--- backend/swa/src/auth/auth.controller.js
+++ backend/swa/src/auth/auth.controller.js
@@ -1,23 +1,29 @@
-import { Bind, Body, Controller, Dependencies, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+// backend/swa/src/auth/auth.controller.js
+import { Bind, Body, Controller, Dependencies, HttpCode, HttpStatus, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { UserService } from '../user/user.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 @Dependencies(AuthService)
 @Dependencies(UserService)
 export class AuthController {
-  constructor(authService,userService) {
+  constructor(authService, userService) {
     this.authService = authService;
     this.userService = userService;
   }
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  @Bind(Request())
-  async login(req) {
+  @Bind(Request(), Body())
+  async login(req, loginDto) {
+    const dto = new LoginDto(loginDto);
+
     return this.authService.login(req.user);
   }
+
   @Post('register')
   @Bind(Body())
   async register(createUserDto) {
