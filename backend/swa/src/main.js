7bc72ff8638fb53edcb5db2cc708e81442f9871c
bmm333 <benmema3@gmail.com>
2025-09-09 03:01:58 +0200

    ok

--- backend/swa/src/main.js
+++ backend/swa/src/main.js
@@ -29,7 +29,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
 
   const port = process.env.PORT || 3001;
-  const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '192.168.1.7';
+  const host = process.env.NODE_ENV === '0.0.0.0'
   await app.listen(port, host);
   console.log(`Server running on http://${host}:${port}`);
 }
