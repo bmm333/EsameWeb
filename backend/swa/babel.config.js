9f707d88e41211c6401270fdb9971db6461337cd
bmm333 <benmema3@gmail.com>
2025-09-09 17:30:36 +0200

    ok?

---
+++ backend/swa/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { 
+      loose: true, 
+      targets: { node: 'current' } 
+    }]
+  ],
+  plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-runtime', { regenerator: true }]
+  ]
+};
